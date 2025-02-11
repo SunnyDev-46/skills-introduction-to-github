64d6c278d452cdc3b16d9ec1b065a9f9664e1482
SunnyDev-46 <namecode062@gmail.com>
2025-02-11 14:23:53 +0800

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,16 @@
+print("budget tracker")
+
+print("1 - add budget")
+print("2 - add expenses")
+print("3 - Summary")
+print("4 - exit")
+user = int(input("select options: "))
+
+if (user == 1):
+    print(1000)
+elif (user == 2):
+    print("Enter expenses")
+
+
+#income = int(input("Enter income"))
+#expenses = int(input(""))2
